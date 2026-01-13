bf10e30349e6dc03f724d8f73c5ef941cd277ed5
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2026-01-13 07:48:41 -0300

    feat: me route & decorators isPublic + activeUserId

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from '../auth/auth.guard';
+import { ActiveUserId } from 'src/shared/decorators/activeUserId';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('me')
   @UseGuards(AuthGuard)
-  me(@Req() request: any) {
-    return this.usersService.getUserById(request.userId);
+  me(@ActiveUserId() userId: string) {
+    return this.usersService.getUserById(userId);
   }
 }
